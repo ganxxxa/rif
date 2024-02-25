3cae55d5414a0b725fd9d0718e67c4e8ce7c3deb
ganxxxa <imanzadmajid@gmail.com>
2024-02-25 10:54:36 +0330

    hgfd

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 "use client";
+import { useState, useEffect } from "react";
 import BlogSection from "@/components/Blog";
 import ConsultationComponent from "@/components/Consultation";
 import Form from "@/components/Form";
@@ -8,11 +9,23 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
+  // State to track if the video is loaded
+  const [isVideoLoaded, setIsVideoLoaded] = useState(false);
+
   return (
     <main>
       <section className="relative max-w-screen h-screen overflow-hidden">
         <div className="relative w-screen h-screen">
-          <div className="text-white absolute h-full w-full  flex sm:items-start items-center text-center sm:text-start justify-center flex-col gap-10  px-8 md:px-24 xl:px-40">
+          {!isVideoLoaded && (
+            <Image
+              src="/lazy.jpg"
+              alt="Background"
+              layout="fill"
+              objectFit="cover"
+              className="h-full w-full object-cover" // Ensuring the image covers the whole section
+            />
+          )}
+          <div className="text-white absolute h-full w-full flex sm:items-start items-center text-center sm:text-start justify-center flex-col gap-10 px-8 md:px-24 xl:px-40">
             <h2 className="font-semibold z-10 text-5xl sm:text-6xl">
               تردد در دنیایی بدون مرز!
             </h2>
@@ -24,7 +37,7 @@ export default function Home() {
             <Link className="z-10" href="/">
               <button
                 type="button"
-                className="border-2  text-md py-2 px-7 rounded-lg"
+                className="border-2 text-md py-2 px-7 rounded-lg"
               >
                 اطلاعات بیشتر
               </button>
@@ -36,6 +49,7 @@ export default function Home() {
             muted
             loop
             className="h-full w-full object-cover"
+            onLoadedData={() => setIsVideoLoaded(true)} // Set the video as loaded
           />
           <div className="absolute inset-0 bg-[#0b2538] bg-gradient-to-tl from-[#0b2538] via-[#0b2538a4] from-10% via-50% to-80% to-[#ffffff6c] opacity-55 "></div>
         </div>
--- src/components/SecondSectionComponent.jsx
+++ src/components/SecondSectionComponent.jsx
@@ -4,13 +4,13 @@ import React from "react";
 
 const SecondSectionComponent = ({ title, paragraph, imageUrl }) => {
   return (
-    <div className="flex flex-col sm:flex-row-reverse w-screen h-96 sm:h-72">
+    <div className="flex flex-col sm:flex-row-reverse p-8 w-screen h-96 sm:h-72">
       <div className="w-full h-full flex flex-col gap-6 justify-center px-4 sm:px-10">
         <h3 className="text-xl">{title}</h3>
         <p>{paragraph}</p>
       </div>
       <div className="relative h-full w-full">
-        <Image fill className=" object-cover" src={imageUrl} />{" "}
+        <Image fill className="rounded-lg object-contain" src={imageUrl} />{" "}
       </div>
     </div>
   );
